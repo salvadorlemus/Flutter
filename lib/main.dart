62ad56099b5c32696a15b79293e9f058b3143ec9
SLemus <salvadorlemus.06@outlook.com>
2023-06-14 21:47:29 -0600

    Added ball images. Added launcher icon for Android and iOS Added logic for the magic ball. Added stateful widget.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,46 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MagicBall());
 }
+
+class MagicBall extends StatefulWidget {
+  const MagicBall({super.key});
+
+  @override
+  State<MagicBall> createState() => _MagicBallState();
+}
+
+class _MagicBallState extends State<MagicBall> {
+  int ballNumber = 1;
+
+  /// Randomize ball textures
+  void RandomBall() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue[300],
+        appBar: AppBar(
+          title: Text('Ask Me Anything'),
+          backgroundColor: Colors.blue[900],
+        ),
+        body: Center(
+          child: TextButton(
+            onPressed: () {
+              RandomBall();
+            },
+            child: Image.asset('images/ball$ballNumber.png')),
+        ),
+      ),
+    );
+  }
+}
+
