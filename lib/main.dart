f6ee0a2ed2ac42b87731eb0d04da93643fd7b09b
SLemus <salvadorlemus.06@outlook.com>
2023-06-15 23:57:05 -0600

    Added a QuizzBrain class. Refactor code to use QuizBrain class. Refactor Question class.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:base/Question.dart';
 import 'package:flutter/material.dart';
+import 'QuizBrain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -29,12 +31,6 @@ class _QuizPageState extends State<QuizPage> {
 
   List <Widget> scoreKeeper = [];
 
- List<Question> questionBank = [
-   Question(q: 'You can lead a cow down stairs but not up stairs.', a: false),
-   Question(q: 'Approximately one quarter of human bones are in the feet.', a: true),
-   Question(q: 'A slug\'s blood is green.', a: true),
-  ];
-
   int questionNumber = 0;
 
   @override
@@ -49,7 +45,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -76,7 +72,7 @@ class _QuizPageState extends State<QuizPage> {
                 onPressed: () {
                   //The user picked true.
 
-                  if(questionBank[questionNumber].questionAnswer == true){
+                  if(quizBrain.questionBank[questionNumber].questionAnswer == true){
                     print('User got it right!');
                   } else {
                     print('User got it wrong!');
@@ -105,7 +101,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
                 onPressed: () {
 
-                  if(questionBank[questionNumber].questionAnswer == false){
+                  if(quizBrain.questionBank[questionNumber].questionAnswer == false){
                     print('User got it right!');
                   } else {
                     print('User got it wrong!');
