ddf4cfc90ed279f91cb15981b1ab6626d5258dad
SLemus <salvadorlemus.06@outlook.com>
2023-06-27 13:25:25 -0600

    Introduce Themes for the app. Refactor main dart to separate the input page widget into their oun file. Create InputPage.dart class.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'inputPage.dart';
 
 void main() => runApp(BMICalculator());
 
@@ -6,30 +7,32 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        // Change app bar theme for the dark theme
+        appBarTheme: const AppBarTheme(
+          color: Color(0xFF0A0E21),
+        ),
+        // Define the primary color for all the app
+        colorScheme: const ColorScheme.dark().copyWith(
+          primary: const Color(0xFF0A0E21),
+        ),
+          // change scaffold background color to the same as the theme
+          scaffoldBackgroundColor: const Color(0xFF0A0E21),
+          // Change the color of the texts
+          textTheme: const TextTheme(
+            bodyLarge: TextStyle(
+              color: Colors.white,
+            ),
+            bodyMedium: TextStyle(
+              color: Colors.white,
+            ),
+            bodySmall: TextStyle(
+              color: Colors.white,
+            ),
+        )
+      ),
       home: InputPage(),
     );
   }
 }
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
