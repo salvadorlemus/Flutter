d52f9cbb5c6ebabb3142740d0b8d287ced69aeaa
SLemus <salvadorlemus.06@outlook.com>
2023-07-05 13:05:42 -0600

    First commit.
    Added starter project files.

--- android/app/src/main/kotlin/com/example/base/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/base/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.example.base
+package com.example.bitcointicker
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,16 @@
 import 'package:flutter/material.dart';
+import 'price_screen.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey[900],
-        appBar: AppBar(
-          title: Text('Mase app'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-              'https://storage.googleapis.com/cms-storage-bucket/d406c736e7c4c57f5f61.png',
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+          primaryColor: Colors.lightBlue,
+          scaffoldBackgroundColor: Colors.white),
+      home: PriceScreen(),
+    );
+  }
 }
-
-// TODO Change app name in the next files :
-// app/src/main/kotlin/com/example/base/MainActivity.kt :: 1
-// app/src/main/AndroidManifest.xml  :: 2 & 4
-// app/src/debug/AndroidManifest.xml :: 2
-// app/build.gradle  :: 47
