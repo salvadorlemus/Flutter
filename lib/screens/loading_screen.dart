77bfc91e40582a6bc9067c1ebf0504981b9260eb
SLemus <salvadorlemus.06@outlook.com>
2023-07-04 18:40:22 -0600

    Move Get position Method to his own class. Refactor loading_screen class to use refactored code. Use try catch blocks to get user position.

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,7 +1,7 @@
 // https://medium.flutterdevs.com/explore-widget-lifecycle-in-flutter-e36031c697d0
 
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -9,49 +9,14 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
-  Future<Position> _determinePosition() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-
-    // Test if location services are enabled.
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
-    if (!serviceEnabled) {
-      // Location services are not enabled don't continue
-      // accessing the position and request users of the
-      // App to enable the location services.
-      return Future.error('Location services are disabled.');
-    }
-
-    permission = await Geolocator.checkPermission();
-    if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
-      if (permission == LocationPermission.denied) {
-        // Permissions are denied, next time you could try
-        // requesting permissions again (this is also where
-        // Android's shouldShowRequestPermissionRationale
-        // returned true. According to Android guidelines
-        // your App should show an explanatory UI now.
-        return Future.error('Location permissions are denied');
-      }
-    }
-
-    if (permission == LocationPermission.deniedForever) {
-      // Permissions are denied forever, handle appropriately.
-      return Future.error(
-          'Location permissions are permanently denied, we cannot request permissions.');
-    }
-
-    // When we reach here, permissions are granted and we can
-    // continue accessing the position of the device.
-    return await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-  }
-
   // This state runs only once when the app is loaded
+
+  Location location = Location();
+
   @override
   void initState() {
     super.initState();
-    _determinePosition().then((value) => print(value));
+    getLocation();
   }
 
   // This state runs every time the app is re-loaded
@@ -65,5 +30,11 @@ class _LoadingScreenState extends State<LoadingScreen> {
   void deactivate() {
     // TODO: implement deactivate
     super.deactivate();
+  }
+
+  void getLocation() async {
+    await location.getCurrentPosition();
+    print('${location.latitude}');
+    print('${location.longitude}');
   }
 }
