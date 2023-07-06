6502e4909ea14a6d38b265b886510aa46ddbf814
SLemus <salvadorlemus.06@outlook.com>
2023-07-06 13:49:27 -0600

    First commit.
    Project initial configuration.
    Imported related assets.

--- android/app/src/main/kotlin/com/example/base/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/base/MainActivity.kt
@@ -1,6 +1,6 @@
-package com.example.base
+package com.example.flashchat
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,21 @@
 import 'package:flutter/material.dart';
+import '../screens/welcome_screen.dart';
+import '../screens/login_screen.dart';
+import '../screens/registration_screen.dart';
+import '../screens/chat_screen.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blueGrey[900],
-        appBar: AppBar(
-          title: Text('Mase app'),
-          backgroundColor: Colors.blueGrey[900],
-        ),
-        body: Center(
-          child: Image(
-            image: NetworkImage(
-              'https://storage.googleapis.com/cms-storage-bucket/d406c736e7c4c57f5f61.png',
-            ),
-          ),
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.black54),
         ),
       ),
-    ),
-  );
+      home: WelcomeScreen(),
+    );
+  }
 }
-
-// TODO Change app name in the next files :
-// app/src/main/kotlin/com/example/base/MainActivity.kt :: 1
-// app/src/main/AndroidManifest.xml  :: 2 & 4
-// app/src/debug/AndroidManifest.xml :: 2
-// app/build.gradle  :: 47
